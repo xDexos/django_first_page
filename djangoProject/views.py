827c8ee1f0798ed375e9eccb4b07d736be57e7f8
Mikołaj Namysł <127437730+xDexos@users.noreply.github.com>
2024-06-15 21:20:56 +0200

    first commit

---
+++ djangoProject/views.py
@@ -0,0 +1,10 @@
+# from django.http import HttpResponse
+from django.shortcuts import render
+
+def homepage(request):
+    # return HttpResponse("Hello World! I'am Home.")
+    return render(request, 'home.html')
+
+def about(request):
+    # return HttpResponse("My About page.")
+     return render(request, 'about.html')
---
+++ static/js/main.js
@@ -0,0 +1,1 @@
+console.log("To jest JS ze strony About.")
